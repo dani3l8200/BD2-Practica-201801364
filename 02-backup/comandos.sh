d173323d63c97c64dc15cf7d3baf6e0f2ef8f468
Juan Daniel <dani3l25948@gmail.com>
2021-06-16 22:32:13 -0600

    feat: :zap: Actualizacion Readme

--- 02-backup/comandos.sh
+++ 02-backup/comandos.sh
@@ -3,3 +3,7 @@ sqlplus -s -l futbol/futbol @"/bbdd/practica/backup/backup_drop.sql" >> "/bbdd/p
 sqlplus -s -l elecciones/elecciones @"/bbdd/practica/autenticacion/ddl.sql" >> "/bbdd/practica/autenticacion/carga.txt"
 sqlplus -s -l elecciones/elecciones @"/bbdd/practica/autenticacion/droppeo.sql" >> "/bbdd/practica/autenticacion/drop.txt"
 sqlplus -s -l elecciones/elecciones @"/bbdd/practica/autenticacion/put_data.sql" >> "/bbdd/practica/autenticacion/put_data.txt"
+expdp futbol/futbol parfile=futbol_equipos.par
+expdp futbol/futbol parfile=futbol_jornadas.par
+impdp futbol/futbol parfile=jornadas.par 
+impdp futbol/futbol parfile=equipos.par
